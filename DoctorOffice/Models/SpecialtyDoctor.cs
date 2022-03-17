79949b39e9e6ac59ce5e4872a4a2785f7cb49d78
John Whitten <johnwhitten.studio@gmail.com>
2022-03-16 20:03:34 -0700

    add specialtydoctor model

---
+++ DoctorOffice/Models/SpecialtyDoctor.cs
@@ -0,0 +1,11 @@
+namespace DoctorOffice.Models
+{
+  public class SpecialtyDoctor
+  {       
+    public int SpecialtyDoctorId { get; set; }
+    public int SpecialtyId { get; set; }
+    public int DoctorId { get; set; }
+    public virtual Specialty Specialty { get; set; }
+    public virtual Doctor Doctor { get; set; }
+  }
+}
